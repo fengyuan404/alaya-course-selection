355a0ffcc054befc35be933febe0d4438d8ef3ea
fengyuan404 <3308099053@qq.com>
2025-11-16 21:39:19 +0800

    feat:初始化Alaya选课系统基础项目

---
+++ src/main/java/com/alaya/coursesystem/alaya_course_selection/AlayaCourseSelectionApplication.java
@@ -0,0 +1,13 @@
+package com.alaya.coursesystem.alaya_course_selection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlayaCourseSelectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlayaCourseSelectionApplication.class, args);
+	}
+
+}
